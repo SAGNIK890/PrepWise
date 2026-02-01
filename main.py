ab87e00a4c5788ce21ea45aafa818dd16ed4b42c
CodingWithSandipta <sandiptabhattacharyya2345@gmail.com>
2026-02-02 01:15:10 +0530

    Fix env loading and MongoDB connection

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from auth import verify_password, create_access_token
 
 load_dotenv()
 
+
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
